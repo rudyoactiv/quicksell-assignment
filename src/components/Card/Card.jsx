6e11629c2e3037e2c74850231cfc4e5990566d5c
Rudraneel Dutta <its.rudraneel@gmail.com>
2024-11-04 20:45:24 +0530

    card logos

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,41 @@ import React from "react";
 import "./Card.css"; // Assuming you have a CSS file for styles
 import cardimg from "../../assets/react.svg";
 
+import priority0 from "../../assets/icons/priority0.svg";
+import priority1 from "../../assets/icons/priority1.svg";
+import priority2 from "../../assets/icons/priority2.svg";
+import priority3 from "../../assets/icons/priority3.svg";
+import priority4 from "../../assets/icons/priority4.svg";
+
+
+
+import backlog from "../../assets/icons/status/backlog.svg";
+import todo from "../../assets/icons/status/todo.svg";
+import progress from "../../assets/icons/status/progress.svg";
+import done from "../../assets/icons/status/done.svg";
+import canceled from "../../assets/icons/status/canceled.svg";
+
+
 const Card = ({ ticket, grouping }) => {
+
+  console.log(ticket.status);
+
+  const priorityImages = {
+    0: priority0,
+    1: priority1,
+    2: priority2,
+    3: priority3,
+    4: priority4,
+  };
+
+  const statusImages = {
+    "Todo": todo,
+    "In progress": progress,
+    "Backlog": backlog,
+    "Done": done,
+    "Canceled": canceled,
+  };
+
   return (
     <div className="card">
       <div className="card-header">
@@ -10,11 +44,14 @@ const Card = ({ ticket, grouping }) => {
         {grouping != "User" && <img src={cardimg} alt="" />}
       </div>
       <div className="card-title">
-        {grouping != "Status" && <img src={cardimg} alt="" />}
+        <img src={statusImages[ticket.status]} alt={`Priority ${ticket.status}`} />
+
         <p>{ticket.title}</p>
       </div>
       <div className="card-footer">
-        {grouping != "Priority" && <img src={cardimg} alt="" />}
+      {grouping !== "Priority" && (
+          <img src={priorityImages[ticket.priority]} alt={`Priority ${ticket.priority}`} />
+        )}
         {/* {ticket.priority} */}
         <div className="tag">
           <div className="circle"></div>
